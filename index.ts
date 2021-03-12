8091c2d24a9a38f3091193cbb259382f1ae9ae03
Anwesh43 <anweshthecool0@gmail.com>
2021-03-12 23:05:13 +0530

    adding SquarePipeOpener class

--- index.ts
+++ index.ts
@@ -185,3 +185,26 @@ class SPONode {
         return this 
     }
 }
+
+class SquarePipeOpener {
+
+    curr : SPONode = new SPONode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
